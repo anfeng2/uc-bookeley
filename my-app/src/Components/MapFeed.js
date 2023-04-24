a2a7b120c5e5507c44f4b8cd72a4e8aee158df53
TrevorBaba <trevor.baba@berkeley.edu>
2023-04-24 15:11:23 -0700

    filter bar

--- my-app/src/Components/MapFeed.js
+++ my-app/src/Components/MapFeed.js
@@ -11,19 +11,53 @@ const MapFeed = () => {
     const center = useMemo(() => ({ lat: 37.8719, lng: -122.2585 }), []);
 
     return (
-    <div className="MapFeed">
-        {!isLoaded ? (
-        <h1>Loading...</h1>
-        ) : (
-        <GoogleMap
-            mapContainerClassName="map-container"
-            center={center}
-            zoom={17}
-        >
-            <Marker position={{ lat: 37.8719, lng: -122.2585 }} />
-        </GoogleMap>
-        )}
-    </div>
+        <div className="container">
+            <div className="filters-container">
+                <div className="filters">
+                    <h2>Filters</h2>
+                    <ul>
+                        <li>
+                            <input type="checkbox" name="food-drink-allowed" id="food-drink-allowed"></input>
+                            <label htmlFor="food-drink-allowed">food/drink allowed</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="cal-id-not-required" id="cal-id-not-required"></input>
+                            <label htmlFor="cal-id-not-required">Cal ID not required</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="quiet-study" id="quiet-study"></input>
+                            <label htmlFor="quiet-study">quiet study</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="room-reservations" id="room-reservations"></input>
+                            <label htmlFor="room-reservations">room reservations</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="printing" id="printing"></input>
+                            <label htmlFor="printing">printing</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="open-now" id="open-now"></input>
+                            <label htmlFor="open-now">open now</label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="MapFeed">
+                {!isLoaded ? (
+                <h1>Loading...</h1>
+                ) : (
+                <GoogleMap
+                    mapContainerClassName="map-container"
+                    center={center}
+                    zoom={17}
+                >
+                    <Marker position={{ lat: 37.8719, lng: -122.2585 }} />
+                </GoogleMap>
+                )}
+            </div>
+        </div>
+        
       );
 }
 export default MapFeed;
