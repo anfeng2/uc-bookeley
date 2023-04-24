e8a6747d10171cc4e47f04316927097e2acb0d0b
Angela Feng <anfeng2@berkeley.edu>
2023-04-24 11:05:41 -0700

    added more backend stuff

--- my-app/backend/routes/library.js
+++ my-app/backend/routes/library.js
@@ -4,21 +4,40 @@ const router = express.Router();
 const Library = require('../models/Library');
 
 
-router.post (
-    "/favorite",
-    async(req, res) => {
-        const library = new Library({
-            library: req.body
-        })
-        library.save((err)=>{
-            if (err){
-                res.status(400).json({error: err, message: "save error"})
-            }else{
-                res.status(200).json({message: "success"})
-            }
-
-        });
+router.route("/favorite")
+    .post(async(req, res) => {
+        const {libraryName} = req.body;
+        const library = new Library({libraryName})
+        try {
+            await library.save();
+            res.status(200).json({libraryName});
+        } catch (err) {
+            console.log("Console.log:", err);
+            res.status(500).send("Error in saving");
+        }
     }
 )
+
+router.route("/unfavorite/:libraryName")
+    .delete(async(req, res) => {
+        try {
+            const library = await Library.findOneAndDelete({libraryName: req.params.libraryName});
+            res.status(200).json(library);
+        } catch (err) {
+            res.status(500).json({message: "Error finding or deleting library"});
+        }
+        
+    }
+)
+
+router.route("/saved")
+    .get(async(req, res) => {
+        try {
+            const libraryJson = await Library.find({libraryName: {$exists: true}});
+            res.status(200).json(libraryJson);
+        } catch (err) {
+            res.status(500).json({message: "Error retrieving favorited libraries"});
+        }
+    })
 
 module.exports = router;
